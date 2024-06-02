fd1623eeb0a5e594d910706c2bab177108ffee1d
romak maharjan <romakmaharjan@gmail.com>
2024-06-02 14:53:05 +0545

    code login error message update

--- app/Http/Controllers/admin/AdminLoginController.php
+++ app/Http/Controllers/admin/AdminLoginController.php
@@ -4,10 +4,42 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AdminLoginController extends Controller
 {
     public function index () {
         return view ('admin.login');
+    }
+
+    public function authenticate(Request $request){
+
+        $validator = validator::make($request->all(),[
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        if ($validator->passes()) {
+            if(auth::guard('admin')->attempt(['email'=> $request->email, 'password'=>
+            $request->password],$request->get('remember'))){
+
+                $admin = Auth::guard('admin')->user();
+
+                if($admin->role==2) {
+                    return redirect()->route('admin.dashboard');
+                }else {
+                    Auth::guard('admin')->logout;
+                    return redirect()->route('admin.login')->with('error','you are not authorized to access admin panel.');
+                }
+
+            }else {
+                return redirect()->route('admin-login')->with('error','Either Email/Password is incorrect');
+            }
+
+        }else{
+            return redirect()->route('admin.login')
+            ->withErrors($validator)
+            ->withInput($request->only('email'));
+        }
     }
 }
